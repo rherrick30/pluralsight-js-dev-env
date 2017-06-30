349e400101c776618e04f1a40fbce37453fb081d
Robert Herrick <rherrick30@gmail.com>
2017-06-30 08:53:50 -0400

    Finished Course

---
+++ buildScripts/build.js
@@ -0,0 +1,34 @@
+﻿/*eslint-disable no-console*/
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import chalk from 'chalk';
+
+//declare we are running node in PRODUCTION MODE
+process.env.NODE_ENV = 'production';
+
+console.log(chalk.blue('Generating minified build for production, please wait...'));
+
+webpack(webpackConfig).run((err, stats) => {
+    if (err) {
+        console.log(chalk.red(err));
+        return 1;
+    }
+
+    //ouput stats, warns, errors to console.
+    const jsonStats = stats.toJson();
+
+    if (jsonStats.hasErrors) {
+        return jsonStats.errors.map(error => console.log(chalk.red(error)));
+    }
+    if (jsonStats.hasWarnings) {
+        console.log(chalk.yellow('Webpack generated the following warnings:'));
+        jsonStats.errors.map(error => console.log(chalk.yellow(error)));
+    }
+
+    console.log(`Webpack stats: ${stats}`);
+
+    //If we got this far the build succeeded!
+    console.log(chalk.green('Your app has been build for production and written to /dist'));
+
+    return 0;
+});
---
+++ webpack.config.prod.js
@@ -0,0 +1,63 @@
+﻿import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+    debug: true,
+    devtool: 'source-map',
+    noInfo: false,
+    entry: {
+        vendor: path.resolve(__dirname, "src/vendor"),
+        main: path.resolve(__dirname, 'src/index')
+    },
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].[chunkhash].js'
+    },
+    plugins: [
+        // Generate an external css filename with a hash in the filename
+        new ExtractTextPlugin('[name].[contenthash].css'),
+
+        //Hash the files using MD5 so that their names change when the content changes
+        new WebpackMd5Hash(),
+
+        // Use CommonsChunkPlugin to create a separate bundle
+        // of vendor libraries so theat they're cached separately
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'  // note it matches the entry key above
+        }),
+
+        //create HTML file that includes reference to bundled JS
+        new HtmlWebpackPlugin({
+            template: 'src/HTMLPage1.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            },
+            inject: true
+        }),
+
+        //Eliminate duplicat packages when generating bundle
+        new webpack.optimize.DedupePlugin(),
+        //Minify JS
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourcemap') }
+        ]
+    }
+}
